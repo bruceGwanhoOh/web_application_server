5b44e4b19923732ea2f2228b81230bd94d824a48
Bruce Oh <bruceo@beflex.co>
2018-08-09 19:45:36 +0900

    first commit wk2

---
+++ src/main/java/com/bruce/brucious_api/BruciousApiApplication.java
@@ -0,0 +1,14 @@
+package com.bruce.brucious_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class BruciousApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruciousApiApplication.class, args);
+    }
+}
